23cb207225812de493911f929bdd1bf57fe9a838
Dmytro Soldatov <cyberdexx@gmail.com>
2020-04-24 21:48:55 +0300

    feat: create app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
+import { Application } from 'pixi.js'
 
+new class Game extends Application {
+    constructor() {
+        super()
+        this.init()
+    }
 
-if (new URLSearchParams(window.location.search).get('debug')) {
-    import('pixi.js').then(PIXI => window['PIXI'] = PIXI)
+    public async init() {
+        // await load config
+        this.ready()
+    }
+
+    private ready() {
+        document.body.appendChild(this.view)
+        window.addEventListener('resize', () => this.onResize())
+        this.onResize()
+    }
+
+    private onResize() {
+        this.view.width = window.innerWidth
+        this.view.height = window.innerHeight
+        this.stage.children.forEach((element: any) => element.resize(this.view.width, this.view.height))
+    }
 }
