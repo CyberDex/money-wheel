299b9c808fd0887b8177c9a7579a36065e86bc97
Dmytro Soldatov <cyberdexx@gmail.com>
2020-04-25 22:13:02 +0300

    config(sceneControlles): update with redux

--- src/controllers/SceneController.ts
+++ src/controllers/SceneController.ts
@@ -3,6 +3,9 @@ import { SplashScene } from '../scenes/SplashScene'
 import { GameScene } from '../scenes/GameScene'
 import { UIScene } from '../scenes/UIScene'
 import { Scenes } from '../helpers/enums/Scenes'
+import { gsap } from "gsap"
+import { States } from '../helpers/enums/States'
+import { store } from 'redux/store'
 
 export class SceneController {
     private scenes: {
@@ -13,14 +16,44 @@ export class SceneController {
         this.addScene(Scenes.UI, new UIScene())
         this.addScene(Scenes.GAME, new GameScene())
         this.addScene(Scenes.SPLASH, new SplashScene())
+
+        store.subscribe(() => this.stateChange())
+    }
+
+    private stateChange() {
+        switch (store.getState().state) {
+            case States.BETTING:
+                this.hide(Scenes.SPLASH)
+                this.show(Scenes.GAME)
+                this.show(Scenes.UI)
+                break;
+            case States.SPIN:
+
+                break;
+            case States.RESULT:
+
+                break;
+            default:
+                break;
+        }
     }
 
     public show(scene: Scenes) {
         this.scenes[scene].visible = true
+        gsap.to(this.scenes[scene], {
+            alpha: 1,
+            duration: .3
+        })
     }
 
     public hide(scene: Scenes) {
-        this.scenes[scene].visible = false
+        gsap.to(this.scenes[scene], {
+            alpha: 0,
+            duration: .3,
+            onComplete: () => {
+                this.scenes[scene].visible = false
+            }
+        })
     }
 
     public resize(w, h: number) {
