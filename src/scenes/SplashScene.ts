ad6d29352cc5027094da03d883228027516985b5
Dmytro Soldatov <cyberdexx@gmail.com>
2020-04-25 15:56:48 +0300

    feat(splashScene): add slider

--- src/scenes/SplashScene.ts
+++ src/scenes/SplashScene.ts
@@ -1,19 +1,27 @@
-import { View } from 'Pixil'
+import { View, Slider } from 'Pixil'
 import { Text, TextStyle } from 'pixi.js'
 import * as lang from '../config/local/en.json'
 import * as styles from '../config/styles.json'
 import { Texts } from 'helpers/enums/Texts'
 
 export class SplashScene extends View {
-    private text: Text
+    private text, balance: Text
+    private slider: Slider
 
     constructor() {
         super()
         this.text = this.addText(lang[Texts.STARTING_BALANCE], styles[Texts.STARTING_BALANCE] as TextStyle)
+        this.balance = this.addText("0", styles[Texts.STARTING_BALANCE] as TextStyle)
+        this.slider = new Slider(0, 0, 200, 40)
+        this.addChild(this.slider)
     }
 
     public resize(w, h: number) {
         this.text.x = w * .5
         this.text.y = h * .35
+        this.balance.x = w * .5
+        this.balance.y = h * .42
+        this.slider.x = w * .5
+        this.slider.y = h * .5
     }
 }
