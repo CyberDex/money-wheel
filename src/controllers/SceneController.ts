f218e278f992eae6e2ea72c805626d438db4e976
Dmytro Soldatov <cyberdexx@gmail.com>
2020-04-26 18:39:37 +0300

    config: small refactor

--- src/controllers/SceneController.ts
+++ src/controllers/SceneController.ts
@@ -1,4 +1,4 @@
-import { App, SceneManager } from 'pixil'
+import { SceneManager } from 'pixil'
 import { SplashScene } from 'scenes/SplashScene'
 import { GameScene } from 'scenes/GameScene'
 import { UIScene } from 'scenes/UIScene'
@@ -6,18 +6,17 @@ import { GameOverScene } from 'scenes/GameOverScene'
 import { store } from 'redux/store'
 import { Scenes } from 'helpers/enums/Scenes'
 import { States } from 'helpers/enums/States'
-import { Actions } from '../helpers/enums/Actions';
 
 export class SceneController extends SceneManager {
-
-    constructor(private readonly app: App) {
+    constructor() {
         super()
-        app.stage.addChild(this)
 
+        this.addScene(Scenes.SPLASH, new SplashScene())
         this.addScene(Scenes.UI, new UIScene())
         this.addScene(Scenes.GAME, new GameScene())
-        this.addScene(Scenes.SPLASH, new SplashScene())
         this.addScene(Scenes.GAME_OVER, new GameOverScene())
+
+        this.showOnlyScene(Scenes.SPLASH)
 
         store.subscribe(() => this.stateChange())
     }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
 import { App } from 'pixil'
 import { SceneController } from 'controllers/SceneController'
-import { Scenes } from 'helpers/enums/Scenes'
 
 new class Game extends App {
-    private sceneController: SceneController
-
     constructor() {
         super({ antialias: true })
-        this.sceneController = new SceneController(this)
-        this.sceneController.showOnlyScene(Scenes.SPLASH)
-        this.resizeHandler()
-    }
-
-    public resizeHandler() {
-        const w = this.renderer.width
-        const h = this.renderer.height
-        this.sceneController.resize(w, h)
+        this.stage.addChild(new SceneController())
+        document.body.appendChild(this.view)
     }
 }
