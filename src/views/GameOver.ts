e4707403b3a4e85d059a9e9b3584e3e23e773d7a
Dmytro Soldatov <cyberdexx@gmail.com>
2020-05-01 13:21:51 +0300

    config(Pixil): update submodule

--- src/views/GameOver.ts
+++ src/views/GameOver.ts
@@ -5,19 +5,20 @@ import { Actions } from '../helpers/enums/Actions';
 import { text } from 'helpers/help'
 import * as style from '../config/styles.json'
 import * as config from '../config/scenes/PopupScene.json'
+import { TextStyle } from 'pixi.js';
 
 export class GameOver extends View {
     private restartButton: Button
 
     constructor() {
         super()
-        this.addChild(new Label(text(Texts.GAME_OVER_TITLE), style.title, config.title.x, config.title.y))
-        this.addChild(new Label(text(Texts.GAME_OVER_TEXT), style.subTitle, config.subtitle.x, config.subtitle.y))
+        this.addChild(new Label(text(Texts.GAME_OVER_TITLE), style.title as TextStyle, config.title.x, config.title.y))
+        this.addChild(new Label(text(Texts.GAME_OVER_TEXT), style.subTitle as TextStyle, config.subtitle.x, config.subtitle.y))
 
         this.restartButton = new Button({
             ...config.button,
             text: text(Texts.GAME_OVER_BUTTON),
-            style: style.button
+            style: style.button as TextStyle
         })
         this.addChild(this.restartButton)
         this.restartButton.onClick(() => {
--- src/views/Splash.ts
+++ src/views/Splash.ts
@@ -6,6 +6,7 @@ import { startGame } from 'redux/actions'
 import * as style from '../config/styles.json'
 import * as config from '../config/scenes/PopupScene.json'
 import * as gameConf from '../config/game.json'
+import { TextStyle } from 'pixi.js'
 
 
 export class Splash extends View {
@@ -15,8 +16,8 @@ export class Splash extends View {
 
     constructor() {
         super()
-        this.addChild(new Label(text(Texts.START_TITLE), style.title, config.title.x, config.title.y))
-        this.balance = new Label(gameConf.minStartBalance, style.subTitle, config.subtitle.x, config.subtitle.y)
+        this.addChild(new Label(text(Texts.START_TITLE), style.title as TextStyle, config.title.x, config.title.y))
+        this.balance = new Label(gameConf.minStartBalance, style.subTitle as TextStyle, config.subtitle.x, config.subtitle.y)
         this.addChild(this.balance)
 
         this.slider = new Slider(
@@ -32,7 +33,7 @@ export class Splash extends View {
         this.startButton = new Button({
             ...config.button,
             text: text(Texts.START_BUTTON),
-            style: style.button
+            style: style.button as TextStyle
         })
         this.addChild(this.startButton)
         this.startButton.onClick(() => {
