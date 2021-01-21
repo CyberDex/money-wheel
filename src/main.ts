26800966eb295a341191fc11e7bf6155f4b53b01
Dmytro Soldatov <cyberdexx@gmail.com>
2021-01-21 23:00:28 +0200

    add some assets for testing

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,35 @@
-import { Game } from 'pixil'
-import { Button, View } from 'pixil'
+import { Game, Scene } from 'pixil'
 
-new class App extends View {
+new class App extends Scene {
   public constructor() {
     super()
 
-    const game = Game.getInstance({ antialias: true })
+    const game = Game.getInstance()
     game.views.add('game', this)
 
-    this.addImg({
-      texture: "sprites/sprites.png",
+    this.addButton({
+      text: 'spin',
       positionX: 50,
-      positionY: 50
+      positionY: 50,
+      color: 0xde3249,
+      width: 150,
+      height: 60,
+      round: 30,
+      style: {
+        fontFamily: "Arial",
+        fontSize: 24,
+        fontWeight: "bold",
+        fill: "#ffffff",
+      },
+      onClick: async () => {
+        await game.loadAssets(["sprites/forest.json"])
+
+        this.addImg({
+          positionX: 10,
+          positionY: 3,
+          texture: "Pause_Button",
+        })
+      }
     })
-
-    this.addChild(
-      new Button({
-        text: 'spin',
-        positionX: 50,
-        positionY: 50,
-        color: 0xde3249,
-        width: 150,
-        height: 60,
-        round: 30,
-        style: {
-          fontFamily: "Arial",
-          fontSize: 24,
-          fontWeight: "bold",
-          fill: "#ffffff",
-        },
-      })
-    )
-  }
-
-  private async loadAssets() {
-    await GAME.loadAssets(["sprites/sprites.json"])
   }
 }
