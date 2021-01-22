1e5b01a505abab73496303dedfce3012440c2755
Dmytro Soldatov <cyberdexx@gmail.com>
2021-01-22 23:56:51 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ new class App extends Scene {
 
   public constructor() {
     super()
-    this.initGame()
   }
 
-  private async initGame() {
+  async init() {
     const game = Game.getInstance()
     game.views.add('game', this)
     await game.loadAssets(["sprites/forest.json"])
@@ -15,32 +14,8 @@ new class App extends Scene {
   }
 
   private initScene() {
-    this.addButton({
-      text: 'spin',
-      positionX: 50,
-      positionY: 50,
-      color: 0xde3249,
-      width: 150,
-      height: 60,
-      round: 30,
-      style: {
-        fontFamily: "Arial",
-        fontSize: 24,
-        fontWeight: "bold",
-        fill: "#ffffff",
-      },
-      onClick: async () => {
-        const img = this.addImg({
-          positionX: 10,
-          positionY: 3,
-          width: 100,
-          height: 100,
-          texture: "Pause_Button",
-        })
-
-        console.log(img);
-
-      }
+    this.addImg({
+      texture: "Log",
     })
   }
 }
