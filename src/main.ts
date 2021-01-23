1b8fbf39cc2c95fdbddb386b90c5062957b2b0b4
Dmytro Soldatov <cyberdexx@gmail.com>
2021-01-23 02:23:16 +0200

    tests

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,31 @@ async function initGame() {
 
   mainScene.addImg({
     texture: "Log",
-    // x: 'center',
-    // y: 'center',
+    y: 'bottom',
     w: '100%',
+  })
+
+  mainScene.addImg({
+    texture: "Carved_Shape",
+    x: '40%',
+    y: 'bottom',
+    w: 300,
+    h: 250,
+  })
+
+  mainScene.addImg({
+    texture: "Carved_Shape",
+    x: '55%',
+    y: 'bottom',
+    w: 300,
+    h: 250,
+  })
+
+  mainScene.addImg({
+    texture: "Carved_Shape",
+    x: '70%',
+    y: 'bottom',
+    w: 300,
+    h: 250,
   })
 }
