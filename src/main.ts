d68fc35c44d32befac142c03b91d57c1429c4e8f
Dmytro Soldatov <cyberdexx@gmail.com>
2021-01-25 22:14:20 +0200

    refactor with resize

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,35 @@
 import { GAME } from 'pixil'
 
-const mainScene = GAME.addScene('main')
-initGame()
+const mainScene = GAME.addScene('main', ["sprites/forest.json"])
+  .then((scene) => {
 
-async function initGame() {
-  await mainScene.loadAssets(["sprites/forest.json"])
+    scene.addImg({
+      bg: "Log",
+      y: 'bottom',
+      w: '100%',
+    })
 
-  mainScene.addImg({
-    texture: "Log",
-    y: 'bottom',
-    w: '100%',
+    // mainScene.addImg({
+    //   texture: "Carved_Shape",
+    //   x: '40%',
+    //   y: 'bottom',
+    //   w: 300,
+    //   h: 250,
+    // })
+
+    // mainScene.addImg({
+    //   texture: "Carved_Shape",
+    //   x: '55%',
+    //   y: 'bottom',
+    //   w: 300,
+    //   h: 250,
+    // })
+
+    // mainScene.addImg({
+    //   texture: "Carved_Shape",
+    //   x: '70%',
+    //   y: 'bottom',
+    //   w: 300,
+    //   h: 250,
+    // })
   })
-
-  mainScene.addImg({
-    texture: "Carved_Shape",
-    x: '40%',
-    y: 'bottom',
-    w: 300,
-    h: 250,
-  })
-
-  mainScene.addImg({
-    texture: "Carved_Shape",
-    x: '55%',
-    y: 'bottom',
-    w: 300,
-    h: 250,
-  })
-
-  mainScene.addImg({
-    texture: "Carved_Shape",
-    x: '70%',
-    y: 'bottom',
-    w: 300,
-    h: 250,
-  })
-}
